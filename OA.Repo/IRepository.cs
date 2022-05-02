79a8d23556a5d9cb80baa1af054e541d852c25bf
Izymryda <witebear200396@gmail.com>
2022-05-02 18:55:37 +0300

    creare a class for repositopy

---
+++ OA.Repo/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OA.Data;
+
+namespace OA.Repo
+{
+    public interface IRepository<T> where T:BaseUserEntity
+    {
+        IEnumerable<T> GetAll();
+        T Get(long id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Remove(T entity);
+        void SaveChanges();
+    }
+}
---
+++ OA.Repo/Repository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OA.Data;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace OA.Repo
+{
+    public class Repository<T> : IRepository<T> where T : BaseUserEntity
+    {
+        private readonly ApplicationContext context;
+        private DbSet<T> entities;
+        string errorMessage = string.Empty;
+
+        public Repository(ApplicationContext context)
+        {
+            this.context = context;
+            entities = context.Set<T>();
+        }
+
+        public void Delete(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Remove(entity);
+            context.SaveChanges();            
+        }
+
+        public T Get(long id)
+        {
+            return entities.SingleOrDefault(s => s.Id == id);
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return entities.AsEnumerable();
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Remove(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Remove(entity);
+        }
+
+        public void SaveChanges()
+        {
+            context.SaveChanges();
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            context.SaveChanges();
+        }
+    }
+}
