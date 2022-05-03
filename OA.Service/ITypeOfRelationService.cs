ad962dfa7d7ec0ac646695bde3fee7d98a008d96
Izymryda <witebear200396@gmail.com>
2022-05-03 22:20:40 +0300

    create a Web layer

---
+++ OA.Service/ITypeOfRelationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OA.Data;
+
+namespace OA.Service
+{
+    public interface ITypeOfRelationService
+    {
+        IEnumerable<TypeOfRelation> GetType();
+        TypeOfRelation GetType(long id);
+        void InsertType(User user);
+        void UpdateType(User user);
+        void DeleteType(long id);
+    }
+}
---
+++ OA.Service/TypeOfRelationService.cs
@@ -0,0 +1,51 @@
+﻿using OA.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using OA.Data;
+using OA.Repo;
+
+namespace OA.Service
+{
+    class TypeOfRelationService : ITypeOfRelationService
+    {
+        //private IRepository<TypeOfRelation> typeRepository;//?
+        /*
+        public TypeOfRelationService(IRepository<TypeOfRelation> typeRepository)
+        {
+            this.typeRepository = typeRepository;
+        }
+        */
+        public void DeleteType(long id)
+        {
+            /*TypeOfRelation typeOfRelation = typeRepository.Get(id);
+            typeRepository.Remove(typeOfRelation);
+            typeRepository.SaveChanges();*/
+            throw new NotImplementedException();
+        }
+
+        public TypeOfRelation GetType(long id)
+        {
+            //return typeRepository.Get(id);
+            throw new NotImplementedException();
+        }
+
+        public void InsertType(User user)
+        {
+            //typeRepository.Insert(user);
+            throw new NotImplementedException();
+        }
+
+        public void UpdateType(User user)
+        {
+            //typeRepository.Update(user);
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<TypeOfRelation> ITypeOfRelationService.GetType()
+        {
+            //return typeRepository.GetAll();
+            throw new NotImplementedException();
+        }
+    }
+}
