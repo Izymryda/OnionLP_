d005bc2c5b0e30735f7e8f543b361d459160eed9
Izymryda <witebear200396@gmail.com>
2022-06-11 12:12:54 +0300

    add a modal windows

--- OA.Data/BaseUserEntity.cs
+++ OA.Data/BaseUserEntity.cs
@@ -7,5 +7,6 @@ namespace OA.Data
         public Int64 Id { get; set; }
         public DateTime AddedDate { get; set; }
         public DateTime ModifiedDate { get; set; }
+        public string IPAddress { get; set; }
     }
 }
