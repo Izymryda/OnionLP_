65900bdc5d88b99e868aa1e67e3f74aad6363ffa
Izymryda <witebear200396@gmail.com>
2022-06-11 10:10:20 +0300

    add user interface

---
+++ OA.Web/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OA.Service;
+
+namespace OA.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService userService;
+        private readonly IUserProfileService userProfileService;
+        public UserController(IUserService userService, IUserProfileService userProfileService)
+        {
+            this.userService = userService;
+            this.userProfileService = userProfileService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<UserViewModel> model = new List<UserViewModel>();
+            userService.GetUsers().ToList().ForEach(u =>
+            {
+                UserProfile userProfile = userProfileService.GetUserProfile(u.Id);
+                UserViewModel user = new UserViewModel
+                {
+                    Id = u.Id,
+                    Name = $"{userProfile.FirstName} {userProfile.LastName}",
+                    Email = u.Email,
+                    Address = userProfile.Address
+                };
+                model.Add(user);
+            });
+
+            return View(model);
+        }        
+    }
+}
